5850bada7dd444d85b4c5b7b4acb5250830bb6aa
xiekaifen <xiekaifen@qiyuanfin.com>
2018-06-29 10:11:19 +0800

    array demo

--- array.go
+++ array.go
@@ -1,5 +1,7 @@
 package leetcode_array
 
+import "sort"
+
 /**
 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
 
@@ -37,3 +39,36 @@ func RemoveDuplicates(nums []int) (int) {
 只出现一次的数字
 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
  */
+func FindSingleElement(nums []int) int {
+	// 题目已经明确是非空数组
+	// 针对数组只有一个元素的情况
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	// 数组有多个元素
+	tmp := 0
+	for _, num := range nums {
+		tmp ^= num
+	}
+	return tmp
+}
+
+
+/**
+给定一个整数数组，判断是否存在重复元素。
+
+如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+ */
+ func containsDuplicate(nums []int) bool {
+	// 首先判断数组长度，== 0 直接返回false
+	if len(nums) == 0 {
+		return false
+	}
+	sort.Ints(nums)
+	for i := 1; i < len(nums); i++ {
+		if nums[i]^nums[i-1] == 0 {
+			return true
+		}
+	}
+ 	return false
+ }
--- array_test.go
+++ array_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"sort"
 	"fmt"
+	"github.com/stretchr/testify/assert"
 )
 //go:generate
 // 测试Int数组·排序
@@ -17,4 +18,34 @@ func TestRemoveDuplicates(t *testing.T){
 	nums := []int{0, 0, 1, 2 ,2 ,3 ,3, 3 ,4 ,4}
 	length:= RemoveDuplicates(nums)
 	fmt.Println(length)
+}
+
+// 按位或运算符"|"是双目运算符。 其功能是参与运算的两数各对应的二进位相或
+func TestMove(t *testing.T){
+	num1 := 1
+	num2 := 1
+	fmt.Println(num1 & num2)
+}
+
+func TestFindSingleElement(t *testing.T) {
+	nums := []int{4,1,2,1,2}
+	tmp := FindSingleElement(nums)
+	fmt.Println(tmp)
+}
+
+//
+func TestContainsDuplicate(t *testing.T){
+	nums1 := []int{1,1,1,3,3,4,3,2,4,2}
+	flag1 := containsDuplicate(nums1)
+	assert.Equal(t, true, flag1)
+	fmt.Println(flag1)
+	nums2 := []int{1,2,3,1}
+	flag2 := containsDuplicate(nums2)
+	assert.Equal(t, true, flag2)
+	fmt.Println(flag2)
+
+	nums3 := []int{1,2,3,4}
+	flag3 := containsDuplicate(nums3)
+	assert.Equal(t, false, flag3)
+	fmt.Println(flag3)
 }
